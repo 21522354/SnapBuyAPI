da42593fe116a47ea687248b741c6fbc90211a52
21522354 <120326395+21522354@users.noreply.github.com>
2025-04-15 21:37:34 +0700

    Set up project for ProductService

---
+++ ProductService/Mapper/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace ProductService.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+        }
+    }
+}
---
+++ ProductService/Models/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductService.Models.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ParentId { get; set; }
+        public Category Parent { get; set; }
+        public string Slug { get; set; }
+        public DateTime CreatedAt { get; set; } 
+    }
+}
